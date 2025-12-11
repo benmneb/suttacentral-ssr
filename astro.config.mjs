098444c108f15e77c7ea0e062dc34fd8e1f409b8
benmneb <git.irritant899@simplelogin.fr>
2025-12-11 17:31:54 +0800

    chore: deploy to cloudfare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 // @ts-check
 import sitemap from '@astrojs/sitemap'
-import vercel from '@astrojs/vercel'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+
+import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: cloudflare(),
   site: 'https://suttacentral.now',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
+    css: {
+      transformer: 'lightningcss',
+    },
   },
 })
