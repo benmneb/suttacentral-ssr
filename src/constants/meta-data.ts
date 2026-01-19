90c27137f0233d820998f5a381c02186eeb978bd
benmneb <git.irritant899@simplelogin.fr>
2026-01-19 17:02:17 +0800

    feat: add site meta-data

---
+++ src/constants/meta-data.ts
@@ -0,0 +1,7 @@
+export const siteMetaData = {
+  title: 'SuttaCentral.now',
+  description: 'A fast and minimal alternative frontend for SuttaCentral.net',
+  source: 'https://sr.ht/~benmneb/suttacentral-ssr',
+  analytics: 'https://dashboard.simpleanalytics.com/suttacentral.now',
+  suggestions: 'https://todo.sr.ht/~benmneb/suttacentral-ssr',
+}
