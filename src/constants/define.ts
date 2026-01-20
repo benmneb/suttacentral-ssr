ddbb2206b0121267628174d63008fba27c05c763
benmneb <git.irritant899@simplelogin.fr>
2026-01-21 06:36:04 +0800

    refactor: make resused const global

---
+++ src/constants/define.ts
@@ -0,0 +1,9 @@
+export const dictionaryTitles = {
+  ncped: 'New Concise Pali English Dictionary',
+  cped: 'Concise Pali English Dictionary',
+  dhammika: 'Nature and the Environment in Early Buddhism by S. Dhammika',
+  dppn: 'Dictionary of Pali Proper Names',
+  pts: 'PTS Pali English Dictionary',
+  dpd: 'Digital Pāḷi Dictionary',
+  ddb: 'Digital Dictionary of Buddhism',
+}
