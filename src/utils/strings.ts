1b93816415214d65ede631096cc91ebe856525fd
benmneb <git.irritant899@simplelogin.fr>
2026-02-14 16:13:56 +0800

    feat: add individual range suttas

--- src/utils/strings.ts
+++ src/utils/strings.ts
@@ -15,3 +15,17 @@ export function toSentenceCase(str: string): string {
     .toLowerCase()
     .replace(/(^\s*\p{L}|[.!?]\s+\p{L})/gu, match => match.toUpperCase())
 }
+
+export function uidToAcronym(uidInput: string) {
+  if (!uidInput) return ''
+  // I think it's only AN and Dhp that have range suttas, but anyway...
+  return String(uidInput).replace(
+    /^([a-zA-Z]+)(.*)$/,
+    (_, l, n) => l.toUpperCase().replace('DHP', 'Dhp') + ' ' + n
+  ) // eg "AN 1.1", "Dhp 1"...
+}
+
+export function uidToTitle(uidInput: string) {
+  if (uidInput.includes('.')) return uidInput.split('.')[1] // "AN 1.1" -> "1" as per .net
+  return uidToAcronym(uidInput).split('Dhp ')[1] // just the number too (.net just leaves the chapter title...)
+}
