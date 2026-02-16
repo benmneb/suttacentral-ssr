85b1160110528a86364b81c55be9f0a28cba7117
benmneb <git.irritant899@simplelogin.fr>
2026-02-17 06:18:40 +0800

    feat: move user prefs from localstorage to cookies
    to avoid layoutshift and give work to the server whenever possible

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,6 +1,7 @@
 import type { AstroCookies } from 'astro'
 
 const SEARCH_LANGUAGES_KEY = 'searchLanguages'
+const VIEW_PREFS_KEY = 'viewPrefs'
 
 export function getSearchLanguages(cookies: AstroCookies): string[] | null {
   const cookieValue = cookies.get(SEARCH_LANGUAGES_KEY)?.json() as
@@ -17,4 +18,13 @@ export function setSearchLanguages(
     path: '/',
     // No maxAge = session cookie (expires when browser closes)
   })
+}
+
+export function getViewPrefs(
+  cookies: AstroCookies
+): Record<string, boolean | string> {
+  const cookieValue = cookies.get(VIEW_PREFS_KEY)?.json() as
+    | Record<string, boolean | string>
+    | undefined
+  return cookieValue ?? {}
 }
