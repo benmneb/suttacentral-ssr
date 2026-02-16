c8600c705e9fb363257294be69b61277f2bd81d0
benmneb <git.irritant899@simplelogin.fr>
2026-02-16 17:04:29 +0800

    chore: disable astro dev toolbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import cloudflare from '@astrojs/cloudflare'
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: { enabled: false },
   output: 'server',
   adapter: cloudflare(),
   site: 'https://suttacentral.now',
